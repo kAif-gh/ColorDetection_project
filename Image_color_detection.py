789406bb61c59e282807ba8f070eb01c97a75467
kAif-gh <80365885+kAif-gh@users.noreply.github.com>
2022-10-08 16:07:59 +0100

    First Commit
    05/10/2022

---
+++ Image_color_detection.py
@@ -0,0 +1,82 @@
+
+
+import cv2
+import pandas as pd
+
+input_img_path ='test-image/132927.jpg'
+img = cv2.imread(input_img_path)
+
+#percent by which the image is resized
+scale_percent = 50
+
+#calculate the 50 percent of original dimensions
+width = int(img.shape[1] * scale_percent / 100)
+height = int(img.shape[0] * scale_percent / 100)
+
+# dsize
+dsize = (width, height)
+
+# resize image
+resized_img = cv2.resize(img, dsize)
+
+# declaring global variables
+clicked = False
+r = g = b = x_pos = y_pos = 0
+
+# Reading csv file with pandas and giving names to each column
+index = ["color", "color_name", "hex", "R", "G", "B"]
+csv = pd.read_csv('DataSet_color/colors.csv', names=index, header=None)
+
+
+# function to calculate minimum distance from all colors and get the most matching color
+def get_color_name(R, G, B):
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R - int(csv.loc[i, "R"])) + abs(G - int(csv.loc[i, "G"])) + abs(B - int(csv.loc[i, "B"]))
+        if d <= minimum:
+            minimum = d
+            cname = csv.loc[i, "color_name"]
+    return cname
+
+
+# function to get x,y coordinates of mouse double click
+def draw_function(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b, g, r, x_pos, y_pos, clicked
+        clicked = True
+        x_pos = x
+        y_pos = y
+        b, g, r = resized_img[y, x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
+
+
+cv2.namedWindow('image')
+cv2.setMouseCallback('image', draw_function)
+
+while True:
+
+
+    if clicked:
+
+        # cv2.rectangle(image, start point, endpoint, color, thickness)-1 fills entire rectangle
+        cv2.rectangle(resized_img, (20, 20), (750, 60), (b, g, r), -1)
+
+        # Creating text string to display( Color name and RGB values )
+        text = get_color_name(r, g, b) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
+
+        # cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
+        cv2.putText(resized_img, text, (50, 50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # For very light colours we will display text in black colour
+        if r + g + b >= 600:
+            cv2.putText(resized_img, text, (50, 50), 2, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
+
+        clicked = False
+    cv2.imshow("image", resized_img)
+    # Break the loop when user hits 'esc' key
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
---
+++ README.md
@@ -0,0 +1,25 @@
+
+# Color Detection Project
+
+Implementing an image color detector which identifies all the colors
+in image and video feed.
+
+# DataSet :
+https://github.com/codebrainz/color-names/blob/master/output/colors.csv
+
+
+
+## Technologies
+
+OpenCV / Pandas
+
+
+## Usage
+
+You have to install the required packages, you can do it:
+* via pip
+```bash
+  pip install -r requirements.txt
+```
+
+
